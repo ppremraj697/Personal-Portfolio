f42cc42e79ce8d42bf34c08a1bb96e9e11a5c2e0
Premraj Patil <ppremraj697@gmail.com>
2024-12-10 12:29:40 +0530

    Updated experience and skills

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-    for (let tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (let tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
 // 2nd script tag
@@ -19,28 +19,28 @@ function opentab(tabname) {
 var sideMenu = document.getElementById("sideMenu");
 
 function openMenu() {
-    sideMenu.style.right = "0";
+  sideMenu.style.right = "0";
 }
 function closeMenu() {
-    sideMenu.style.right = "-200px";
+  sideMenu.style.right = "-200px";
 }
-
 
 //3rd script tag
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwVALkiTAHGNNzRt_TJYNNY12Wz77DJe2cKZF9yycT4Hq4Itz1p0qtCdumq_-Q_hsJw/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwVALkiTAHGNNzRt_TJYNNY12Wz77DJe2cKZF9yycT4Hq4Itz1p0qtCdumq_-Q_hsJw/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = "";
-            }, 5000);
-            form.reset();
-        })
-        .catch(error => console.error('Error!', error.message))
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
